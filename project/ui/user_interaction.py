243a34815ce0276c6c4c1eb1d5489483de0ce5b3
shay <atlan.shay19@gmail.com>
2024-07-30 21:38:23 +0300

    FIX AND ADD NEW CODE!!

--- project/ui/user_interaction.py
+++ project/ui/user_interaction.py
@@ -15,7 +15,7 @@ else:
             number_of_mines_input = input(f"{name}, for board size {board_size}, choose number of mines to allocate")
             if number_of_mines_input.isdigit() or (number_of_mines_input.startswith('-') and number_of_mines_input[1:].isdigit()):
                 number_of_mines = int(number_of_mines_input)
-                if number_of_mines > 0 and number_of_mines <= (board_size // 2) and number_of_mines is not None:
+                if number_of_mines > 0 and number_of_mines <= (board_size* board_size // 2) and number_of_mines is not None:
                     print(f"{name}, the board size is: {board_size}, number of mines is: {number_of_mines}")
                 else:
                     print(f"{name}, you have entered illegal number of mines")
