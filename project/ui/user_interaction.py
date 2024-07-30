57183fc1b91d7075246f2d3228fd35c98df79623
shay <atlan.shay19@gmail.com>
2024-07-30 19:56:42 +0300

    FIX AND ADD NEW CODE

--- project/ui/user_interaction.py
+++ project/ui/user_interaction.py
@@ -1,4 +1,7 @@
 # THIS CODE IS WRONG, FIX IT AND ADD NEW CODE
+name = None
+board_size = None
+number_of_mines = None
 name = input("Hello, whats your name")
 if name <= "2":
     print("Your name is too short")
