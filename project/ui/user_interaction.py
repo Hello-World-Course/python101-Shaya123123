d7f86aa698044e5e6aa48bf071d80c87f9465b93
shay <atlan.shay19@gmail.com>
2024-07-30 18:38:49 +0300

    FIX AND ADD NEW CODE

--- project/ui/user_interaction.py
+++ project/ui/user_interaction.py
@@ -1,4 +1,5 @@
 # THIS CODE IS WRONG, FIX IT AND ADD NEW CODE
-name = input("Hello, whats your name")
-board_size = int(input(f"{name}, please choose board size"))
-print(f"{name}, the board size is: {board_size}, choose number of mines to allocate")
+name = input("Hello, what's your name? ")
+board_size = int(input(f"{name}, please choose board size: "))
+num_mines = int(input("Please enter the number of mines: "))
+print(f"{name}, the board size is: {board_size}, number of mines is: {num_mines}")
