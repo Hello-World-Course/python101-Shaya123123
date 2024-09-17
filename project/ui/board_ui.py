601461e24ec10986d738f838cfdae14a12e62a43
shay <atlan.shay19@gmail.com>
2024-09-17 20:06:25 +0300

    add def1+safe_set_value

--- project/ui/board_ui.py
+++ project/ui/board_ui.py
@@ -1,5 +1,8 @@
 def parse_cmd(command):
     command_parts = command.split(" ")
     command_name = command_parts[0]
-    parameters = " ".join(command_parts[1:])
+    if len(command_parts) > 1:
+        parameters = " ".join(command_parts[1:])
+    else:
+        parameters = None
     return command_name, parameters
