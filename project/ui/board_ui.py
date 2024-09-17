2c1766d2ee78b05053d4695ac887ba0d207f202e
shay <atlan.shay19@gmail.com>
2024-09-17 20:21:00 +0300

    add def1sss

--- project/ui/board_ui.py
+++ project/ui/board_ui.py
@@ -2,7 +2,7 @@ def parse_cmd(command):
     command_parts = command.split(" ")
     command_name = command_parts[0]
     if len(command_parts) > 1:
-        parameters = " ".join(command_parts[1:])
+        parameters = [int(digit) for part in command_parts[1:] for digit in part if digit.isdigit()]
     else:
         parameters = []
     return [command_name, parameters]
