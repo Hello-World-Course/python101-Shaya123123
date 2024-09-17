e2ffd18d21613a8de33d3c785643b5ef74df3bab
shay <atlan.shay19@gmail.com>
2024-09-17 19:57:27 +0300

    add def1+safe_set_value

--- project/board/board_functions.py
+++ project/board/board_functions.py
@@ -1,4 +1,13 @@
 def is_on_board(x, y, board):
     # This is wrong fix it:
-    if len(board) > x > -1:
+    if 0 <= x < len(board[0]) and 0 <= y < len(board):
         return True
+    else:
+        return False
+
+def safe_set_value(x, y, value, board):
+    if is_on_board(x, y, board):
+        board[x][y] = value
+        return True
+    else:
+        return False
